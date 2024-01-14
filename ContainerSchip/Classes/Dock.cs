069ae5867b98d468d4c2896977d0b473295e58aa
SenorBlub <ThomasVerhappen@Gmail.com>
2024-01-14 13:25:33 +0100

    added logic to dock

--- ContainerSchip/Classes/Dock.cs
+++ ContainerSchip/Classes/Dock.cs
@@ -32,9 +32,87 @@ namespace ContainerSchip.Classes
             return ships;
         }
 
-        public void PutContainersOnShips()
+        public List<Ship> FindShipsToUse()
         {
+            int valuableContainerCount = 0;
+            int coolableContainerCount = 0;
+            int valuableCoolableContainerCount = 0;
 
+            Dictionary<int, List<Ship>> shipsByWidthDictionary = new Dictionary<int, List<Ship>>();
+            foreach (var container in containers)
+            {
+                switch ((container.valuable, container.coolable))
+                {
+                    case (true, true):
+                        valuableCoolableContainerCount++;
+                        break;
+                    case (true, false):
+                        valuableContainerCount++;
+                        break;
+                    case (false, true):
+                        coolableContainerCount++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            foreach (var ship in ships)
+            {
+                if (shipsByWidthDictionary.ContainsKey(ship.Width))
+                {
+                    shipsByWidthDictionary[ship.Width].Add(ship);
+                }
+                else
+                {
+                    shipsByWidthDictionary.Add(ship.Width, new List<Ship>
+                    {
+                        ship
+                    });
+                }
+
+            }
+
+            var sortedWidths = shipsByWidthDictionary.Keys.OrderByDescending(x => x).ToList();
+
+            int currentCapacity = 0;
+            List<Ship> usedShips = new List<Ship>();
+
+            foreach (var width in sortedWidths)
+            {
+
+                foreach (var ship in shipsByWidthDictionary[width])
+                {
+                    if (currentCapacity >= valuableCoolableContainerCount)
+                    {
+                        break; 
+                    }
+
+                    currentCapacity += width;
+                    usedShips.Add(ship);
+
+                    if (currentCapacity >= valuableCoolableContainerCount)
+                    {
+                        break;
+                    }
+                }
+
+                if (currentCapacity >= valuableContainerCount)
+                {
+                    break; 
+                }
+            }
+
+            if (coolableContainerCount <= 3 * valuableCoolableContainerCount && valuableContainerCount <= valuableCoolableContainerCount)
+            {
+                return usedShips;
+            }
+            else
+            {
+
+            }
+
+            
         }
     }
 
---
+++ ContainerShipTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ContainerShipTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
